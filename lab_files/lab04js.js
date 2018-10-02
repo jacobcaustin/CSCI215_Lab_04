a9363d1e260e375a0cc08474ea287ca79d8b364b
Jacob Austin <austinjc@g.cofc.edu>
2018-10-01 23:22:00 -0400

    this another commit

---
+++ lab_files/lab04js.js
@@ -0,0 +1,56 @@
+
+var I = []
+I[0] = "http://munsellb.people.cofc.edu/img/pound.jpeg";
+I[1] = "http://munsellb.people.cofc.edu/img/euro.jpeg";
+I[2] = "http://munsellb.people.cofc.edu/img/kroon.jpeg";
+I[3] = "http://munsellb.people.cofc.edu/img/canadian.jpeg";
+I[4] = "http://munsellb.people.cofc.edu/img/yuan.jpeg";
+
+// -----------------------------------
+// Global Variables Go Here
+
+function convert() {
+    var dollar = document.getElementsByName("dollars")[0].value;
+    var selectElement = document.getElementById('currency');
+    var exchangeRate = selectElement.value;
+    var index = selectElement.value;
+    var currency = selectElement.options[index].text;
+
+
+
+}
+
+function updateExchange( dollar, exchangeRate, currency, index );
+
+// -----------------------------------
+// Convert Function Goes Here
+
+function calculate(dollar, exchangeRate) {
+
+    return (dollar * exchangeRate);
+}
+
+// -----------------------------------
+// Calculate Function Goes Here
+
+
+// -----------------------------------
+// Functions that are given
+// Do not modify
+
+function updateImage( index ) {
+
+    var url = 'url(' + I[index] + ')';
+    document.getElementById('bill_img').style.backgroundImage=url;
+
+}
+
+function updateExchange( dollar, exchangeRate, currency, index ) {
+
+    document.getElementsByName("exchange")[0].value = calculate( dollar, exchangeRate ).toPrecision(4) + ' ' + currency;
+
+    updateImage( index );
+
+}
+
+
